6a80fc10159ec27f6193e07dfc1239db5a5a35fe
ptdeboer <p.deBoer@esciencecenter.nl>
2013-12-17 10:13:18 +0100

    Initial commit of Platinum UI classes

---
+++ source/nl.esciencecenter.ptk.ui/src/nl/esciencecenter/ptk/ui/widgets/URIDropTargetLister.java
@@ -0,0 +1,11 @@
+package nl.esciencecenter.ptk.ui.widgets;
+
+import java.net.URI;
+import java.util.List;
+
+public interface URIDropTargetLister 
+{
+
+	void notifyUriDrop(List<URI> uriList);
+
+}
