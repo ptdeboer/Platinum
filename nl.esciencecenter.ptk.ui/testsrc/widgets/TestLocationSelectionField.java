9ee1fcbf94d8865e92330b6c66aa86f74cafec3a
ptdeboer <p.deboer@esciencecenter.nl>
2014-02-19 17:19:54 +0100

    Updated DnD methods.

--- nl.esciencecenter.ptk.ui/testsrc/widgets/TestLocationSelectionField.java
+++ nl.esciencecenter.ptk.ui/testsrc/widgets/TestLocationSelectionField.java
@@ -5,8 +5,11 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import nl.esciencecenter.ptk.ui.dnd.DnDFlavors;
 import nl.esciencecenter.ptk.ui.widgets.LocationSelectionField;
+import nl.esciencecenter.ptk.ui.widgets.URIDropHandler;
 import nl.esciencecenter.ptk.ui.widgets.LocationSelectionField.LocationType;
+import nl.esciencecenter.ptk.util.logging.ClassLogger;
 
 public class TestLocationSelectionField
 {
@@ -17,6 +20,9 @@ public class TestLocationSelectionField
         panel.setLayout(new BorderLayout());
         frame.getContentPane().add(panel); 
         //
+        ClassLogger.getLogger(DnDFlavors.class).setLevelToDebug();
+        ClassLogger.getLogger(URIDropHandler.class).setLevelToDebug();
+        
         LocationSelectionField selField=new LocationSelectionField(LocationType.DirType);
         selField.setLocationText("file:/thisisalongdirectoryname/directory/subdirectory/"); 
         panel.add(selField,BorderLayout.CENTER);
