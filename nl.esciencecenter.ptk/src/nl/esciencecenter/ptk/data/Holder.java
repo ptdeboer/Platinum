1096e323840aa8e66cf7e226d613fec35b43f00f
ptdeboer <p.deBoer@esciencecenter.nl>
2014-03-25 18:36:37 +0100

    Holder<T> class implements VARHolder<T> interface.

---
+++ nl.esciencecenter.ptk/src/nl/esciencecenter/ptk/data/Holder.java
@@ -0,0 +1,25 @@
+package nl.esciencecenter.ptk.data;
+
+public class Holder<T> implements VARHolder <T> 
+{
+    public T value; 
+    
+    @Override
+    public boolean isSet()
+    {
+        return (value!=null); 
+    }
+
+    @Override
+    public void set(T newValue)
+    {
+        value=newValue; 
+    }
+
+    @Override
+    public T get()
+    {
+        return value; 
+    }
+
+}
