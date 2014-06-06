1de0be7365ccff324b6f7ac007caa440c200f8ee
ptdeboer <p.deBoer@esciencecenter.nl>
2014-06-06 17:08:43 +0200

    Placeholder.

--- nl.esciencecenter.ptk/testsrc/nl/esciencecenter/ptk/task/Test_MonitorStats.java
+++ nl.esciencecenter.ptk/testsrc/nl/esciencecenter/ptk/task/Test_MonitorStats.java
@@ -1,6 +1,30 @@
 package nl.esciencecenter.ptk.task;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Test_MonitorStats
 {
 
+    @Test 
+    public void testCreateEmpty()
+    {
+        TaskMonitorAdaptor taskMonitor=new TaskMonitorAdaptor(); 
+        MonitorStats empty=new MonitorStats(taskMonitor);
+        
+        // Assert defaults
+        Assert.assertEquals("NILL MonitorStats getETA() should return -1",-1,empty.getETA()); 
+        Assert.assertEquals("NILL MonitorStats getTotalDoneTime() should return -1",-1, empty.getTotalDoneTime());
+    }
+    
+    @Test 
+    public void testCreateNull()
+    {
+        MonitorStats empty=new MonitorStats(null);
+        
+        // Assert defaults
+        Assert.assertEquals("NULL MonitorStats getETA() should return -1",-1,empty.getETA()); 
+        Assert.assertEquals("NULL MonitorStats getTotalDoneTime() should return -1",-1, empty.getTotalDoneTime());
+    }
+    
 }
