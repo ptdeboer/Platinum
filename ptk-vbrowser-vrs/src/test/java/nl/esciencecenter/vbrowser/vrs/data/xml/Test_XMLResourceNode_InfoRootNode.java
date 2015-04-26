97b0b32ed1e0ebbf4488a305307331ce7f753903
Piter T. de Boer <Piter.de.Boer@ordina.nl>
2015-04-26 21:44:17 +0200

    Changed Junit version.

--- ptk-vbrowser-vrs/src/test/java/nl/esciencecenter/vbrowser/vrs/data/xml/Test_XMLResourceNode_InfoRootNode.java
+++ ptk-vbrowser-vrs/src/test/java/nl/esciencecenter/vbrowser/vrs/data/xml/Test_XMLResourceNode_InfoRootNode.java
@@ -1,14 +1,13 @@
 package nl.esciencecenter.vbrowser.vrs.data.xml;
 
-import junit.framework.Assert;
 import nl.esciencecenter.vbrowser.vrs.VRSClient;
 import nl.esciencecenter.vbrowser.vrs.VRSContext;
-import nl.esciencecenter.vbrowser.vrs.data.xml.XMLData;
 import nl.esciencecenter.vbrowser.vrs.infors.InfoResourceNode;
 import nl.esciencecenter.vbrowser.vrs.infors.InfoRootNode;
 import nl.esciencecenter.vbrowser.vrs.infors.Test_InfoRS;
 import nl.esciencecenter.vbrowser.vrs.vrl.VRL;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Test_XMLResourceNode_InfoRootNode
@@ -17,40 +16,39 @@ public class Test_XMLResourceNode_InfoRootNode
     @Test
     public void testRootNodeToXML() throws Exception
     {
-        VRSClient vrsClient=Test_InfoRS.initTestClient();
-        InfoRootNode rootNode = vrsClient.getInfoRootNode(); 
+        VRSClient vrsClient = Test_InfoRS.initTestClient();
+        InfoRootNode rootNode = vrsClient.getInfoRootNode();
         Assert.assertNotNull(rootNode);
-        
-        XMLData data=new XMLData(new VRSContext()); 
-        String xml=data.toXML(rootNode);
+
+        XMLData data = new XMLData(new VRSContext());
+        String xml = data.toXML(rootNode);
         System.out.printf(XMLData.prettyFormat(xml, 3));
-        
+
     }
 
     @Test
     public void testRootNodeSubNodesToXML() throws Exception
     {
-        VRSClient vrsClient=Test_InfoRS.initTestClient();
-        VRSContext ctx=vrsClient.getVRSContext(); 
-        
-        InfoRootNode rootNode = vrsClient.getInfoRootNode(); 
-    
-        rootNode.addResourceLink("links","GFTP eslt007.local", new VRL("http://eslt007.local:2811/~"), null); 
-        rootNode.addResourceLink("links","SFTP eslt007.local", new VRL("https://eslt007.local:22/~"), null); 
-        rootNode.addResourceLink("links","LocalHome:/~", new VRL("file:///~"), null);
-        
-        rootNode.addResourceLink(null,"GFTP eslt007.local", new VRL("http://eslt007.local:2811/~"), null); 
-        rootNode.addResourceLink(null,"SFTP eslt007.local", new VRL("https://eslt007.local:22/~"), null); 
-        rootNode.addResourceLink(null,"LocalHome:/~", new VRL("file:///~"), null);
-        
-        XMLData data=new XMLData(ctx); 
-        String xml=data.toXML(rootNode);
+        VRSClient vrsClient = Test_InfoRS.initTestClient();
+        VRSContext ctx = vrsClient.getVRSContext();
+
+        InfoRootNode rootNode = vrsClient.getInfoRootNode();
+
+        rootNode.addResourceLink("links", "GFTP eslt007.local", new VRL("http://eslt007.local:2811/~"), null);
+        rootNode.addResourceLink("links", "SFTP eslt007.local", new VRL("https://eslt007.local:22/~"), null);
+        rootNode.addResourceLink("links", "LocalHome:/~", new VRL("file:///~"), null);
+
+        rootNode.addResourceLink(null, "GFTP eslt007.local", new VRL("http://eslt007.local:2811/~"), null);
+        rootNode.addResourceLink(null, "SFTP eslt007.local", new VRL("https://eslt007.local:22/~"), null);
+        rootNode.addResourceLink(null, "LocalHome:/~", new VRL("file:///~"), null);
+
+        XMLData data = new XMLData(ctx);
+        String xml = data.toXML(rootNode);
         System.out.printf(XMLData.prettyFormat(xml, 3));
 
         InfoResourceNode copyFolder = rootNode.createFolder("XMLCopy");
         data.addXMLResourceNodesTo(copyFolder, xml);
-        
-        
+
     }
-    
+
 }
