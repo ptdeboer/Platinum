135a24d58ce63e8cdf013a855da1d15d367439cf
Piter T. de Boer <ptdeboer@gmail.com>
2024-03-23 11:00:08 +0100

    Java 21 fix.

--- README.md
+++ README.md
@@ -4,13 +4,10 @@ Platinum
 Swing based GUI Toolkit containing the new VBrowser 2.0 API.\
 From 2013 to 2015 this was the follow-up from the VL-e's VBrowser project.
 
-See here for the Wiki:
-
-- https://github.com/NLeSC/Platinum/wiki
-
-Java 11 notice
+Java 11 (21) notice
 ---
-This project has been upgraded to a Java 11 project.
+This project has been upgraded to a Java 11 project.\
+It also compiles under Java 21 (Since 2024).
 
 Build
 ---
--- scripts/release.sh
+++ scripts/release.sh
@@ -10,14 +10,32 @@ usage()
    echo "usage: <RELEASE_VERSION> <NEW_SNAPSHOT>"
 }
 
+fatal()
+{
+   echo "FATAL:""$@"
+   exit -1
+}
+
 doMvn() {
  echo "MVN:$@"   
  mvn "$@"
+ ret="$?"
+ if [ "${ret}" == "0" ] ; then 
+   return;
+ else
+    fatal "mvn $@ failed with error:${ret}" 
+ fi
 }
 
 doGit() { 
  echo "GIT:$@"   
  git "$@"
+ ret="$?"
+ if [ "${ret}" == "0" ] ; then 
+   return;
+ else
+    fatal "git $@ failed with error:${ret}" 
+ fi
 }
 
 if [ -z "${VERSION}" ] ; then 
@@ -35,19 +53,25 @@ if [[ "${SNAPSHOT}" != *-SNAPSHOT ]] ; then
     exit 1
 fi
 
+# Update/build/verify to VERSION
 doGit checkout develop
 doMvn versions:set -DnewVersion=${VERSION}
-doGit add pom.xml */pom.xml
+doMvn clean verify
+doGit add pom.xml **/pom.xml
 doGit commit -m "New release version=${VERSION}"
 
-doGit checkout release
+# Create/merge/deploy master to VERSION
+doGit checkout master
 doGit merge develop
 doGit tag "v${VERSION}"
-#doGit push origin release
-#doGit push origin "v${VERSION}"
+doGit push origin master
+doGit push origin "v${VERSION}"
+doMvn clean package verify deploy
 
+# Merge back/update develop to SNAPSHOT
 doGit checkout develop
 doMvn versions:set -DnewVersion=${SNAPSHOT}
-doGit add pom.xml */pom.xml
+doGit add pom.xml **/pom.xml
 doGit commit -m "New develop version=${SNAPSHOT}"
-#doGit push origin
+doGit push origin
+
